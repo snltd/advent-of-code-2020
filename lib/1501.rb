952bea9d2394eb5363d2a186c68a8f7cd696104b
Robert Fisher <services@id264.net>
2020-12-15 14:01:10 +0000

    day 15, part 1

---
+++ lib/1501.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require_relative 'support/base'
+
+# Problem 1501
+#
+class Advent1501 < Base
+  def solve
+    solve_for(input.first.split(',').map(&:to_i), 3000000)
+  end
+
+  def solve_for(list, stop_at)
+    return list.last if list.size == stop_at
+
+    last = list.pop # modifies list!
+    new = list.include?(last) ? list.size - list.rindex(last) : 0
+
+    solve_for(list.append(last, new), stop_at)
+  end
+
+  def input_style
+    :as_lines
+  end
+end
---
+++ spec/1501_spec.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+require_relative 'spec_helper'
+require_relative '../lib/1501'
+
+# Problem 1501
+#
+class TestAdvent1501 < MiniTest::Test
+  include SpecHelper
+
+  def sample_answer
+    436
+  end
+end
